0a5b9482ce146f4650bb401236f609d7baf37dfd
gabrielaragaoo <eng.gabrielaragao@gmail.com>
2023-04-14 11:22:26 -0300

    feat: add "no data found" screen

--- EvercodeApp/Screens/Components/EmptyView.swift
+++ EvercodeApp/Screens/Components/EmptyView.swift
@@ -1,6 +1,33 @@
 import UIKit
 
 class EmptyView: UIView {
-    
-    // TODO
+
+    private lazy var emptyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No data found"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(emptyLabel)
+
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            emptyLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            emptyLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
 }
